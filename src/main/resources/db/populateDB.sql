92c0885c4e92368fa7b3436402d1b380d45e7545
David <homazdavid99@Gmail.com>
2026-02-24 22:56:43 +0300

    Implemented Spring JDBC Template in class JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,12 +1,16 @@
 DELETE FROM user_role;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password)
-VALUES ('User', 'user@yandex.ru', 'password'),
-       ('Admin', 'admin@gmail.com', 'admin'),
-       ('Guest', 'guest@gmail.com', 'guest');
+INSERT INTO users (id, name, email, password)
+VALUES (100000,'User', 'user@yandex.ru', 'password'),
+       (100001,'Admin', 'admin@gmail.com', 'admin'),
+       (100002,'Guest', 'guest@gmail.com', 'guest');
 
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (id, user_id, date_time, description, calories)
+VALUES (100003, 100001, '2026-02-23 09:00:00', 'burger', 900);
