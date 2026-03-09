c7fb4752332026f3a4b3a9901cbc6bd2107d7254
David <homazdavid99@Gmail.com>
2026-03-09 19:37:02 +0300

    Implements DataJpaMealRepository, separated repository implementations by Spring profiles, created test (jdbc, jpa, datajpa)

---
+++ src/test/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+@ActiveProfiles("datajpa")
+public class DataJpaUserRepositoryTest extends UserServiceTest {
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+@ActiveProfiles("jdbc")
+public class JdbcUserRepositoryTest extends UserServiceTest {
+
+}
