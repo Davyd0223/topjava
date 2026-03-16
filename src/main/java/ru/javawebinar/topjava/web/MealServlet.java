af85abb98033a82a42adfb812b796ff58357851e
David <homazdavid99@Gmail.com>
2026-03-16 10:53:36 +0300

    6_11_spring_web

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,19 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 public class MealServlet extends HttpServlet {
+
     private ClassPathXmlApplicationContext springContext;
     private MealRestController mealController;
 
@@ -16,6 +23,18 @@ public class MealServlet extends HttpServlet {
 //       springContext.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
         springContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION);
         springContext.refresh();
+        WebApplicationContext springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
         mealController = springContext.getBean(MealRestController.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
     }
 }
