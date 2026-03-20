6ef76e83b545f5475c9677212a62a9cd36b9c588
David <homazdavid99@Gmail.com>
2026-03-20 14:51:14 +0300

    Fixed change

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -6,7 +6,7 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
-    private static int id = 1;
+    private static int id = AbstractBaseEntity.START_SEQ;
 
     private SecurityUtil() {
     }
